99cf92358b653e4657ee9ea84a5930bc99736a8b
Olaf Bergner <olaf.bergner@gmx.de>
2015-05-19 16:36:27 +0200

    Header is only one (1) byte plus remaining length.

--- protocol/src/main/scala/io/wallee/protocol/MqttPacket.scala
+++ protocol/src/main/scala/io/wallee/protocol/MqttPacket.scala
@@ -4,7 +4,7 @@ abstract class MqttPacket {
 
   def lengthInBytes: Int = {
     val remainingLengthBytes: Int = (scala.math.log(remainingLength) / scala.math.log(128)).floor.toInt
-    return 2 + remainingLengthBytes + remainingLength
+    return 1 + remainingLengthBytes + remainingLength
   }
 
   protected def remainingLength: Int
